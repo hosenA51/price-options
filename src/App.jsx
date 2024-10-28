4d6c76c10aaedc417bbb37225c2f5152ee0cf247
hosenA51 <hosenahmed2552@gmail.com>
2024-10-28 19:19:55 +0600

    added rechart js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import LineChart from './components/LineChart/LineChart'
 import NavBar from './components/NavBar/NavBar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 // import DaisyNav from './components/DaisyNav/DaisyNav'
@@ -10,6 +11,7 @@ function App() {
       <NavBar></NavBar>
       {/* <DaisyNav></DaisyNav> */}
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </>
   )
 }
---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { LineChart as Lchart, Line, XAxis, YAxis } from 'recharts';
+const LineChart = () => {
+    const studentsMarksData = [
+        { student: "Student 1", math: 78, physics: 82, chemistry: 74 },
+        { student: "Student 2", math: 85, physics: 88, chemistry: 79 },
+        { student: "Student 3", math: 92, physics: 91, chemistry: 84 },
+        { student: "Student 4", math: 67, physics: 75, chemistry: 70 },
+        { student: "Student 5", math: 88, physics: 89, chemistry: 85 },
+        { student: "Student 6", math: 74, physics: 80, chemistry: 78 },
+        { student: "Student 7", math: 81, physics: 83, chemistry: 76 },
+        { student: "Student 8", math: 90, physics: 95, chemistry: 88 },
+        { student: "Student 9", math: 79, physics: 78, chemistry: 72 },
+        { student: "Student 10", math: 95, physics: 92, chemistry: 89 }
+    ];
+
+    return (
+        <div>
+            <Lchart width={800} height={400} data={studentsMarksData}>
+                <XAxis dataKey="student"></XAxis>
+                <YAxis></YAxis>
+                <Line dataKey="math" stroke='red'></Line>
+                <Line dataKey="physics" stroke='yellow'></Line>
+                <Line dataKey="physics" stroke='white'></Line>
+            </Lchart>
+        </div>
+    );
+};
+
+export default LineChart;
